3fb63aa011e73bdfb581eb41c8b899a658f440db
Jason Maliquid <jasonm2822@gmail.com>
2025-01-17 13:12:23 +0800

    ✨Implement advice generation feature with API integration.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,19 +6,42 @@
 	import Dice from '@/assets/images/icon-dice.svg';
 	import { Button } from '@/components/ui/button';
 	let { children } = $props();
+
+	let isAdviceGenerated: boolean = $state(false);
+	let advice: string = $state('');
+	let adviceId: number = $state(0);
+
+	const generateAdvice = async () => {
+		if (!isAdviceGenerated) {
+			isAdviceGenerated = true;
+		}
+		const url = await fetch('https://api.adviceslip.com/advice'); // Fetching API
+		const response = await url.json(); // Parsing JSON
+		const { slip } = response; // Destructuring the response
+		advice = slip.advice; // Extracting the advice
+		adviceId = slip.id; // Extracting the advice id
+	};
 </script>
 
 <div class="flex h-svh w-svw items-center justify-center bg-cyan-950 px-10 md:px-60">
 	<Card.Root class="relative border-0 bg-white/40 text-center">
 		<Card.Header>
-			<Card.Title class="text-base font-bold uppercase tracking-widest text-green-500 md:text-xl"
-				>Advice #</Card.Title
-			>
+			<Card.Title class="text-base font-bold uppercase tracking-wider text-green-500 md:text-xl">
+				Advice #<span>{isAdviceGenerated ? adviceId : 0}</span>
+			</Card.Title>
 		</Card.Header>
 		<Card.Content class="text-2xl font-semibold tracking-wide text-white">
-			" Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto nihil similique laboriosam
-			harum magni saepe expedita voluptas natus iure magnam nostrum eligendi odio, esse quidem
-			animi, autem sapiente ratione ab. "
+			{#if !isAdviceGenerated}
+				Generate Advice
+			{:else}
+				{#await generateAdvice()}
+					Generating Advice...
+				{:then slip}
+					<blockquote>
+						" {advice} "
+					</blockquote>
+				{/await}
+			{/if}
 		</Card.Content>
 		<Card.Footer class="flex flex-col">
 			<div class="w-full pb-8">
@@ -27,6 +50,7 @@
 			</div>
 			<Button
 				size="icon"
+				onclick={generateAdvice}
 				class="absolute -bottom-8 size-14 rounded-full bg-green-500 hover:bg-green-400 hover:shadow-2xl hover:shadow-green-300"
 			>
 				<img src={Dice} alt="Dice" />
